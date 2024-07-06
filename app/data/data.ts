5272cee6101f39fabe73e449f09a43912a153e2a
Prince Eze <pchidireeze@gmail.com>
2024-07-06 16:46:09 +0100

    feat: finish up page sections

--- app/data/data.ts
+++ app/data/data.ts
@@ -3,6 +3,17 @@ interface Link {
   dropdown: boolean;
 }
 
+interface promoItems {
+  color: string;
+  image: string;
+}
+
+export const fSearch: string[] = [
+  "Rechargeable Fan",
+  "iPhone 15 Pro Max",
+  "Lexus Es 350",
+];
+
 export const links: Link[] = [
   {
     name: "Categories",
@@ -23,5 +34,20 @@ export const links: Link[] = [
   {
     name: "Suppliers",
     dropdown: false,
+  },
+];
+
+export const promoItems: promoItems[] = [
+  {
+    color: "C26900",
+    image: "/Img.png",
+  },
+  {
+    color: "1B5779",
+    image: "/Img-1.png",
+  },
+  {
+    color: "000000",
+    image: "/Img-2.png",
   },
 ];
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,81 @@
 import Image from "next/image";
+import whiteLogo from "@/public/whiteLogo.svg";
+import hero from "@/public/hero.jpg";
+import Link from "next/link";
+import { fSearch, promoItems } from "./data/data";
 
-export default function Home() {
+export default function Page() {
   return (
     <div>
-      <div>
-        image
-        <p>Learn More about Maukal</p>
+      <div
+        className="flex h-[692px] flex-col justify-center px-10 text-white"
+        style={{ backgroundImage: `url(${hero.src})` }}
+      >
+        <div className="flex items-center gap-5 pb-1">
+          <Image src={whiteLogo} alt="logo" />
+          <p className="text-4xl font-medium leading-[50.40px] text-white">
+            Welcome to Maukal
+          </p>
+        </div>
+        <h1 className="max-w-[820px] pb-6 text-7xl font-semibold leading-[100.80px]">
+          The Leading Online Store with the Largest Product
+        </h1>
+        <Link href="./" about="Login" className="pb-6">
+          <button
+            className="flex items-center justify-center gap-2.5 rounded-lg bg-[#FE7F0A] px-[18px] py-2.5 text-2xl font-semibold text-white shadow-[0px_1px_12px_rgba(0,0,0,0.25)]"
+            type="button"
+          >
+            <span>Shop Now</span>
+          </button>
+        </Link>
+        <div className="flex items-center gap-6">
+          <p className="text-2xl">Frequently Searched:</p>
+          <ol className="flex list-none gap-6">
+            {fSearch.map((item) => {
+              return (
+                <li
+                  key={item}
+                  className="rounded-[100px] border border-white px-6 py-2.5 hover:bg-white hover:text-[#CC7F51]"
+                >
+                  {item}
+                </li>
+              );
+            })}
+          </ol>
+        </div>
       </div>
-      <h1>The Leading Online Store with the Largest Product</h1>
-      <button>Shop Now</button>
-      <div>
-        <p>Frequently Searched</p>
-      </div>
+
+      <section className="flex gap-3 p-10">
+        {promoItems.map((item) => {
+          return (
+            <div
+              key={item.image}
+              className="flex text-white"
+              style={{ backgroundColor: "#" + item.color }}
+            >
+              <div className="flex w-1/2 flex-col justify-between py-4 pl-5">
+                <div className="flex flex-col gap-2">
+                  <p className="text-2xl font-semibold leading-[140%]">
+                    New Deals at Best Prices
+                  </p>
+                  <span>From $50.00</span>
+                </div>
+                <a href="" className="underline">
+                  Browse Deals
+                </a>
+              </div>
+              <div className="flex w-1/2 items-center">
+                <Image
+                  src={item.image}
+                  width={300}
+                  height={300}
+                  alt="item.image"
+                />
+              </div>
+            </div>
+          );
+        })}
+      </section>
     </div>
   );
 }
