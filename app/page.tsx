4d92876c8b9d5d0977485a57d6ae8e0c8a313ef8
Prince Eze <pchidireeze@gmail.com>
2024-07-06 14:01:49 +0100

    start working on hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Page</div>;
+  return (
+    <div>
+      <div>
+        image
+        <p>Learn More about Maukal</p>
+      </div>
+      <h1>The Leading Online Store with the Largest Product</h1>
+      <button>Shop Now</button>
+      <div>
+        <p>Frequently Searched</p>
+      </div>
+    </div>
+  );
 }
