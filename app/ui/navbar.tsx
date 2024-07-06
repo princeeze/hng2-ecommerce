9a8ee325c346bdcf7a590d8003c7ee00296c9629
Prince Eze <pchidireeze@gmail.com>
2024-07-06 11:10:43 +0100

    feat: add navbar

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,3 +1,42 @@
+import Image from "next/image";
+import logo from "@/public/logo.svg";
+import camSearch from "@/public/cam.svg";
+import search from "@/public/search.svg";
+import globe from "@/public/globe.svg";
+import profile from "@/public/profile.svg";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex justify-between px-10 py-6">
+      <div>
+        <div>
+          <Image src={logo} alt="logo" />
+        </div>
+        <div className="focus-within:ring-2 hover:ring-2">
+          <input
+            type="text"
+            placeholder="Enter your search"
+            tabIndex={0}
+          ></input>
+          <button type="button">
+            <Image src={camSearch} alt="camera logo" />
+          </button>
+          <button type="button">
+            <span>Search</span>
+            <Image src={search} alt="search logo" />
+          </button>
+        </div>
+      </div>
+      <div className="align-center flex">
+        <a href="">
+          <Image src={globe} alt="globe logo" />
+          <span>Eng</span>
+        </a>
+        <a href="">
+          <Image src={profile} alt="profile logo" />
+        </a>
+        <button>Sign up</button>
+      </div>
+    </nav>
+  );
 }
