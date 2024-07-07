5596235bbf805f4567649e7fd4535c6a541578b3
Prince Eze <pchidireeze@gmail.com>
2024-07-07 04:59:28 +0100

    style: add more sections to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import whiteLogo from "@/public/whiteLogo.svg";
 import hero from "@/public/hero.jpg";
+import ps5 from "@/public/playstation.png";
 import Link from "next/link";
-import { fSearch, promoItems } from "./data/data";
+import { fSearch, promoItems, catItems } from "./data/data";
 
 export default function Page() {
   return (
@@ -45,37 +46,129 @@ export default function Page() {
         </div>
       </div>
 
-      <section className="flex gap-3 p-10">
-        {promoItems.map((item) => {
-          return (
-            <div
-              key={item.image}
-              className="flex text-white"
-              style={{ backgroundColor: "#" + item.color }}
-            >
-              <div className="flex w-1/2 flex-col justify-between py-4 pl-5">
-                <div className="flex flex-col gap-2">
-                  <p className="text-2xl font-semibold leading-[140%]">
-                    New Deals at Best Prices
-                  </p>
-                  <span>From $50.00</span>
+      <main className="p-10">
+        <section className="flex flex-wrap gap-3 pb-16">
+          {promoItems.map((item) => {
+            return (
+              <div
+                key={item.image}
+                className="flex shrink grow basis-64 text-white"
+                style={{ backgroundColor: "#" + item.color }}
+              >
+                <div className="flex w-1/2 flex-col justify-between py-4 pl-5">
+                  <div className="flex flex-col gap-2">
+                    <p className="text-2xl font-semibold leading-[140%]">
+                      New Deals at Best Prices
+                    </p>
+                    <span>From $50.00</span>
+                  </div>
+                  <a href="" className="underline">
+                    Browse Deals
+                  </a>
                 </div>
-                <a href="" className="underline">
-                  Browse Deals
+                <div className="flex w-1/2 items-center">
+                  <Image
+                    src={item.image}
+                    width={300}
+                    height={300}
+                    alt="item.image"
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </section>
+
+        <section>
+          <h2>Popular Categories</h2>
+          <div className="grid grid-cols-6 gap-3">
+            {catItems.slice(0, 12).map((item, index) => {
+              return (
+                <a
+                  key={index}
+                  className="flex flex-col items-center justify-between bg-[#D9D9D9] px-3 pt-2 hover:bg-slate-300"
+                  href=""
+                >
+                  <div className="flex items-center justify-center">
+                    <Image
+                      src={`/cat${index + 1}.png`}
+                      width={150}
+                      className="px-2 py-3"
+                      height={150}
+                      alt={`cat${index + 1}.png`}
+                    />
+                  </div>
+                  <span className="pb-2 text-2xl font-medium">Earpod</span>
                 </a>
-              </div>
-              <div className="flex w-1/2 items-center">
-                <Image
-                  src={item.image}
-                  width={300}
-                  height={300}
-                  alt="item.image"
-                />
-              </div>
-            </div>
-          );
-        })}
-      </section>
+              );
+            })}
+          </div>
+        </section>
+
+        <section className="mt-14 flex justify-between bg-black text-white">
+          <div className="flex max-w-[760px] flex-col justify-center p-12">
+            <h2 className="pb-2 text-5xl font-semibold leading-[67.20px]">
+              PS 5 SONY DELIVERS TO YOU WITH A SPEED OF LIGHT
+            </h2>
+            <p className="max-w-[70%] pb-7 text-2xl font-light leading-8">
+              Worldwide shopping. We ship to over 200 countries right to your
+              doorstep
+            </p>
+            <Link href="./" about="Login">
+              <button
+                className="flex items-center justify-center gap-2.5 rounded-lg bg-[#FE7F0A] px-[18px] py-2.5 text-2xl font-semibold text-white shadow-[0px_1px_12px_rgba(0,0,0,0.25)]"
+                type="button"
+              >
+                <span>Order Now</span>
+              </button>
+            </Link>
+          </div>
+          <div className="pr-12 pt-12">
+            <Image src={ps5} alt="playstation5" />
+          </div>
+        </section>
+
+        <section>
+          <h2>All Products</h2>
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {catItems.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className={`border-[rgba(25,27,31,0.2)]; flex flex-col rounded-[10px] border border-solid bg-white`}
+                  style={{
+                    filter: "drop-shadow(0px 8px 12px rgba(25, 27, 31, 0.1))",
+                  }}
+                >
+                  <div className="flex h-80 w-full items-center justify-center">
+                    <Image
+                      src={`/cat${index + 1}.png`}
+                      width={150}
+                      className="px-2 py-3"
+                      height={150}
+                      alt={`cat${index + 1}.png`}
+                    />
+                  </div>
+                  <div>
+                    <span className="pb-2 text-2xl font-medium">Earpod</span>
+                    <div>
+                      <div>
+                        <span>Oraimo</span>
+                        <div>rating</div>
+                        <div>
+                          <span>price</span>
+                          <span>discount</span>
+                        </div>
+                      </div>
+                      <div>image</div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
